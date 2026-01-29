0e869bca21cbf2c8fbc02868758356d68d767147
Qpham <phamngocquy97@gmail.com>
2026-01-29 11:10:49 +0800

    add

--- 04_concurrent_flow.py
+++ 04_concurrent_flow.py
@@ -6,6 +6,10 @@ logging.basicConfig(level=logging.INFO)
 
 _logger = logging.getLogger(__name__)
 
+@task()
+def spawn_elevator_floors():
+    return 20
+
 @task
 def stop_at_floor(floor: int) -> int:
     _logger.info(f"elevator moving to floor {floor}")
@@ -21,14 +25,15 @@ def total_floors(order_floors: list[int]) -> list[int]:
 def elevator():
     res = []
     floors = []
-    for floor in range(10, 0, -1):
+    total_floors_count = spawn_elevator_floors()
+    for floor in range(total_floors_count, 0, -1):
         floors.append(stop_at_floor.submit(floor))
 
     for f in floors:
         res.append(f.result())
 
-    resp = total_floors.map(res)
-    _logger.info(f"Total floors visited: {resp}")
+    res = total_floors.map()
+    _logger.info(f"Total floors visited: {res}")
 
 if __name__ == "__main__":
     elevator()
